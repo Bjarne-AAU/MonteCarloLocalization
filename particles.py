e07977f9d5365d81d869331b3571a987d8b35798
Bjarne <bjarne@m-tech.aau.dk>
2017-04-05 13:50:55 +0200

    Added basic particles

--- particles.py
+++ particles.py
@@ -3,8 +3,33 @@ import numpy as np
 from Robot import Robot
 
 
-class ParticleGrid(object):
+class AbstractParticles(object):
+
+    def __init__(self):
+        pass
+
+
+
+class ParticlesGrid(AbstractParticles):
 
     def __init__(self, size):
         self._particles = np.zeros(size)
 
+
+class Particles(AbstractParticles):
+
+    def __init__(self, N, map):
+        width = map.width
+        height = map.height
+        # self._particles = map.
+
+    @property
+    def weights(self):
+        return self._particles[:,2]
+
+    @property
+    def positions(self):
+        return self._particles[:,0:2]
+
+    def at(self, index):
+        return self._particles[index,:]
