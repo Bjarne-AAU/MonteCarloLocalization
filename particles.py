794f094a8b2b97c2b33b18f451788ddf91a5f80e
Bjarne <bjarne@m-tech.aau.dk>
2017-04-05 15:38:53 +0200

    Initial sampling

--- particles.py
+++ particles.py
@@ -19,9 +19,10 @@ class ParticlesGrid(AbstractParticles):
 class Particles(AbstractParticles):
 
     def __init__(self, N, map):
+        self.N = N
         width = map.width
         height = map.height
-        # self._particles = map.
+        self._particles = np.hstack((np.random.randint(0, width, (N,1)), np.random.randint(0, height, (N,1))))
 
     @property
     def weights(self):
@@ -30,6 +31,10 @@ class Particles(AbstractParticles):
     @property
     def positions(self):
         return self._particles[:,0:2]
+
+    @positions.setter
+    def positions(self, positions):
+        self._particles[:,0:2] = positions
 
     def at(self, index):
         return self._particles[index,:]
